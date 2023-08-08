2b2a498ad7c801ff716e1bd637d9c43f95e8caed
augusto <augusto@add3.io>
2023-08-08 15:27:40 +0200

    fix: enriched test cases

--- contracts/AddStakingV1.sol
+++ contracts/AddStakingV1.sol
@@ -43,6 +43,8 @@ contract AddStakingV1 {
       stakes[msg.sender].lastUpdate = block.timestamp;
       totalStakes += amount;
   }
+  // 1- This does not follow the Check-Effects-Interaction safety protocol https://fravoll.github.io/solidity-patterns/checks_effects_interactions.html
+  // 2- Is a bit difficult to understand why to send rewards and stake tokens one after the other. Isn't better to account the gained rewards and let the user withdraw separately? 
 
   function unstake(uint256 amount) public {
       require(amount > 0, "Amount must be greater than zero");
@@ -59,6 +61,14 @@ contract AddStakingV1 {
       // transfer token from smart contract to user
       token.safeTransfer(msg.sender, amount);
   }
+  // see above point 1-
+  // 2- Why to make two transfers ? 
+  // 
+  // can't you do ...
+  //
+  // uint256 reward = calculateRewards(msg.sender);
+  // ////////THEN//////// 
+  // token.safeTransfer(msg.sender, amount + rewards); // if rewards is zero is not added to the value, if is something is all sent in once transaaction. This saves gass
 
   function claimRewards() public {
       uint256 reward = calculateRewards(msg.sender);
@@ -68,10 +78,15 @@ contract AddStakingV1 {
       }
       stakes[msg.sender].lastUpdate = block.timestamp;
   }
-
+  // 1- Sorry but where do you account that the user has withdrawn yet the rewards yet? I think that this function call is at reentrancy risk
+  // 2- This way of coding does not follow the Check-Effect-Interaction safe protocol
+  
   function calculateRewards(address account) public view returns (uint256) {
       uint256 timeSinceLastUpdate = block.timestamp.sub(stakes[account].lastUpdate);
       // if reward rate is 100 and staked 365 days, reward amount is 100% of staked amount
       return stakes[account].amount.mul(rewardRate).mul(timeSinceLastUpdate).div(365 days).div(100);
   }
+  // This works only with static rate (fixed staking)
+  // How do you handle the dynamic rate ? 
+
 }
